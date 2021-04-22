c0f36ad495ce48490050ffc6b40b634a4240ffda
Carlos Brito <cbrito1994@gmail.com>
2021-04-22 12:19:23 -0500

    Grabbed all the elements I'm gonna need from the html

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const baseURL = "http://api.openweathermap.org/data/2.5/weather";
 const forecastURL = "https://api.openweathermap.org/data/2.5/onecall";
+const inputForm = document.querySelector('.citySearch__form');
+const mainInfo = document.querySelector('.searchDetails__city');
+const searchedCities = document.querySelector('.searchedCities');
+const forecastInfo = document.querySelector('.forecastInfo');

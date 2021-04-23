e54d6833a88b7a846bde14521054c0cbfa329177
Carlos Brito <cbrito1994@gmail.com>
2021-04-22 22:54:09 -0500

    Corrected a typo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const baseURL = "http://api.openweathermap.org/data/2.5/weather";
+const baseURL = "https://api.openweathermap.org/data/2.5/weather";
 const forecastURL = "https://api.openweathermap.org/data/2.5/onecall";
 const inputForm = document.querySelector('.citySearch__form');
 const mainInfo = document.querySelector('.searchDetails__city');
